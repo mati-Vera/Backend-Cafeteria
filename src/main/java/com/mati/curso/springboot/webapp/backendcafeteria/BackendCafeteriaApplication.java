41b02d74d72a9c8a5d64e06eb5527594ddd9d448
Matias Vera <matias.vera@alumnos.frm.utn.edu.ar>
2025-07-15 18:07:53 -0300

    Primer commit del proyecto Backend-Cafeteria

---
+++ src/main/java/com/mati/curso/springboot/webapp/backendcafeteria/BackendCafeteriaApplication.java
@@ -0,0 +1,13 @@
+package com.mati.curso.springboot.webapp.backendcafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCafeteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCafeteriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mati/curso/springboot/webapp/backendcafeteria/BackendCafeteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mati.curso.springboot.webapp.backendcafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCafeteriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
